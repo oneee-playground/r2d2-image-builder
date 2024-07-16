f20eaf593e6a9c256fa647e72e35afbe6057755f
onee-only <kimww0306@gmail.com>
2024-07-16 11:57:10 +0900

    Tmemporaraly disable all the code

--- function/build.go
+++ function/build.go
@@ -2,18 +2,11 @@ package function
 
 import (
 	"context"
-	"path/filepath"
-	"strings"
 	"time"
 
-	"github.com/go-git/go-billy/v5/osfs"
 	"github.com/google/uuid"
-	"github.com/oneee-playground/r2d2-image-builder/config"
 	"github.com/oneee-playground/r2d2-image-builder/event"
-	"github.com/oneee-playground/r2d2-image-builder/git"
-	"github.com/oneee-playground/r2d2-image-builder/image"
 	"github.com/oneee-playground/r2d2-image-builder/util"
-	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -36,41 +29,41 @@ func HandleBuildImage(ctx context.Context, req BuildImageRequest) (e error) {
 
 	start := time.Now()
 
-	logrus.Info("Creating source directory")
-	fs := osfs.New(filepath.Join(config.Tmpfs, "source"))
+	// logrus.Info("Creating source directory")
+	// fs := osfs.New(filepath.Join(config.Tmpfs, "source"))
 
-	logrus.Infof("Fetching source from: %s:%s", req.Repository, req.CommitHash)
+	// logrus.Infof("Fetching source from: %s:%s", req.Repository, req.CommitHash)
 
-	err := git.FetchSource(ctx, fs, req.Repository, req.CommitHash)
-	if err != nil {
-		logrus.Error(err.Error())
-		return event.Publish(ctx, req.ID, since(start), errors.Wrap(err, "failed to fetch source"))
-	}
+	// err := git.FetchSource(ctx, fs, req.Repository, req.CommitHash)
+	// if err != nil {
+	// 	logrus.Error(err.Error())
+	// 	return event.Publish(ctx, req.ID, since(start), errors.Wrap(err, "failed to fetch source"))
+	// }
 
-	logrus.Info("Building image from source")
+	// logrus.Info("Building image from source")
 
-	img, err := image.Build(ctx, image.BuildOpts{
-		ContextPath: fs.Root(),
-		Platform:    req.Platform,
-	})
-	if err != nil {
-		logrus.Error(err.Error())
-		return event.Publish(ctx, req.ID, since(start), errors.Wrap(err, "failed to build image"))
-	}
+	// img, err := image.Build(ctx, image.BuildOpts{
+	// 	ContextPath: fs.Root(),
+	// 	Platform:    req.Platform,
+	// })
+	// if err != nil {
+	// 	logrus.Error(err.Error())
+	// 	return event.Publish(ctx, req.ID, since(start), errors.Wrap(err, "failed to build image"))
+	// }
 
-	logrus.Info("Pushing image")
+	// logrus.Info("Pushing image")
 
-	err = image.Push(ctx, img, image.PushOpts{
-		Auth:         config.RegistryAuth,
-		RegistryName: config.RegistryAddr,
-		RegistryUser: config.RegistryUser,
-		Repository:   req.TaskID.String(),
-		Tag:          strings.Join([]string{req.Repository, req.CommitHash}, "-"),
-	})
-	if err != nil {
-		logrus.Error(err.Error())
-		return event.Publish(ctx, req.ID, since(start), errors.Wrap(err, "failed to push image"))
-	}
+	// err = image.Push(ctx, img, image.PushOpts{
+	// 	Auth:         config.RegistryAuth,
+	// 	RegistryName: config.RegistryAddr,
+	// 	RegistryUser: config.RegistryUser,
+	// 	Repository:   req.TaskID.String(),
+	// 	Tag:          strings.Join([]string{req.Repository, req.CommitHash}, "-"),
+	// })
+	// if err != nil {
+	// 	logrus.Error(err.Error())
+	// 	return event.Publish(ctx, req.ID, since(start), errors.Wrap(err, "failed to push image"))
+	// }
 
 	return event.Publish(ctx, req.ID, since(start), nil)
 }
